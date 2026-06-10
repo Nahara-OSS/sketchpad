b0211f694a1ff42b8a8ce12914afc34d512187bc
nahkd123 <nahkd123@gmail.com>
2026-06-10 11:07:45 +0700

    Add brush editor to miscellaneous settings page
    We only temporarily put brush editor there for now

--- app/src/main/java/io/github/naharaoss/skpd/settings/ui/MiscellaneousSettingsPage.kt
+++ app/src/main/java/io/github/naharaoss/skpd/settings/ui/MiscellaneousSettingsPage.kt
@@ -1,5 +1,6 @@
 package io.github.naharaoss.skpd.settings.ui
 
+import android.content.Intent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -9,8 +10,10 @@ import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import io.github.naharaoss.skpd.R
+import io.github.naharaoss.skpd.brush.BrushEditorActivity
 import io.github.naharaoss.skpd.settings.MiscellaneousSettings
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
@@ -20,6 +23,8 @@ fun MiscellaneousSettingsPage(
     settings: MiscellaneousSettings,
     onSettingsChange: (MiscellaneousSettings) -> Unit
 ) {
+    val context = LocalContext.current
+
     Column(modifier.verticalScroll(rememberScrollState())) {
         ListItem(
             onClick = {},
@@ -31,6 +36,15 @@ fun MiscellaneousSettingsPage(
                     onCheckedChange = { onSettingsChange(settings.copy(promotionalArtworks = it)) }
                 )
             }
+        )
+
+        ListItem(
+            onClick = {
+                val intent = Intent(context, BrushEditorActivity::class.java)
+                context.startActivity(intent)
+            },
+            content = { Text("Open brush editor") },
+            supportingContent = { Text("Manage and edit brushes") }
         )
     }
 }
